74f953ded9e7a7f66d2f278e72a81adb2f84bee6
Urvashi2409 <urvashicode2410@gmail.com>
2023-07-15 15:45:39 +0530

    two classes till now

---
+++ First.java
@@ -0,0 +1,159 @@
+import java.lang.*;
+import java.util.*;
+
+class First{
+    public static void main(String args[]){
+        // Displaying something 
+        // System.out.println("Hello World");
+        // System.out.print("Print something");
+        // System.out.print("Second line");
+
+        // Scanner scn = new Scanner(System.in);
+        // int x = scn.nextInt();
+        // System.out.println(x);
+
+        // String str = scn.next();
+        // System.out.println(str);
+
+        // String str1 = scn.nextLine();
+        // System.out.println(str1);
+
+        // float f = scn.nextFloat();
+        // System.out.println(f);
+
+        // System.out.println(args[0]);
+        // System.out.println(args[2]);
+
+        // variables 
+        // byte x = 123;
+        // System.out.println(x);
+
+        // short y = 12345;
+        // System.out.println(y);
+
+        // int z = 1234567891;
+        // System.out.println(z);
+
+        // long a = 1234567890;
+        // System.out.println(a);
+
+        // double d = 1223453535335353.345678912342424242444354364364;
+        // float f = 12.3f;
+        // System.out.println(f);
+        // System.out.println(d);
+
+        // boolean b = true;
+        // System.out.println(b);
+
+        // int x = 12;
+        // int age = scn.nextInt();
+
+        // relational operators <, >, ==
+        // if(age < 18){
+        //     System.out.println("You cannot vote!");
+        // }
+        // else if(age == 18){
+        //     System.out.println("You are 18, go apply for voter id!");
+        // }
+        // else{
+        //     System.out.println("You can vote");
+        // }
+
+        // if(age >=18){
+        //     System.out.println("You can vote >= 18");
+        // }
+        // if(age >=21){
+        //     System.out.println("You can vote >=21");
+        // }
+        // else{
+        //     System.out.println("You cannot vote");
+        // }
+
+        // int age = 12;
+        // conditional statements 
+
+        // logical operators: &&, ||, !
+        // && - TT = true 
+        // || - TF, FT, TT = true 
+
+        // boolean operator = &, |, ~, <<, >>
+
+        // age = 12 
+        // if(!(age >= 17)){
+        //     System.out.println("You can vote >= 18");
+        // }
+        // if(age == 0){
+        //     System.out.println("0");
+        // }
+        // else if(age == 12){
+        //     System.out.println("12");
+        // }
+        // else if(age >=21){
+        //     System.out.println("You can vote >=21");
+        // }
+        // else{
+        //     System.out.println("You cannot vote");
+        // }
+
+        // boolean operator = & 
+        // System.out.println(4&6); 
+        // System.out.println(4|6);
+        // System.out.println(~6);
+
+        // +, -, ++, --, /, *, % = Arithematic operators = decimals  
+
+        // System.out.println(6+7);
+        // System.out.println(6-7);
+        // System.out.println(6*7);
+        // System.out.println(16/7); //integer part 
+        // System.out.println(((float)16/7));
+        // System.out.println(15%2);
+
+        // System.out.println(6*2);
+        // System.out.println(6<<1);
+
+        // System.out.println(6/2);
+        // System.out.println(6>>1);
+
+        // unary operators 
+        // int x = 34; 
+        // x = x+1;
+        // x++;
+        // System.out.println(x);
+
+        // x = x-1; 
+        // x--;
+        // System.out.println(x);
+
+        // relational operators = >, <, ==, <=, >= 
+
+        // assignment operators = '=', +=, -=, /=, *=, %=
+
+        // int x = 4;
+        // x += 1;
+        // x = x + 1; 
+        // x++;
+
+        // x -= 1; 
+        // x = x - 1;
+        // x--;
+
+        // x /= 3;
+        // x = x/3; 
+
+        // x *= 3;
+        // x = x * 3;
+
+        // x %= 3;
+        // x = x % 3;
+
+        // done with operators!! 
+
+        // boolean operators 
+
+        // decimal to boolean 
+        // boolean to decimal 
+
+        
+    }
+}
